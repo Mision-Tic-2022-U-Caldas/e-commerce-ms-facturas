ef234ad07fc9e475d3a68f1dce945829ea3ef06b
unknown <maykenm7@gmail.com>
2022-11-10 19:06:20 -0500

    Estructura de datasource, repository, models, relation y controladores para el microservicio de factura

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,5 @@
+export * from './factura-producto-factura.controller';
+export * from './factura.controller';
 export * from './ping.controller';
+export * from './producto-factura-factura.controller';
+
